d1513a8a7d67eebd5f2979e6ee9f50cd6d24c903
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-03-31 16:00:37 +0700

    update

--- std_thread_practice/main.cpp
+++ std_thread_practice/main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-	std::thread t;
+	std::thread t1(sample_thread_func);
+	std::thread t2((SampleWorker()));
+	
+	auto f = []() { for (int i = 0; i < 10; i++) std::cout << std::this_thread::get_id() << std::endl; };
+	std::thread t3(f);
+
+	t1.join();
+	t2.join();
+	t3.join();
+
 	return 0;
 }
--- std_thread_practice/shared.hpp
+++ std_thread_practice/shared.hpp
@@ -4,3 +4,23 @@
 #include <thread>
 
 using namespace std;
+
+class SampleWorker
+{
+public:
+	void operator()()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			std::cout << std::this_thread::get_id() << std::endl;
+		}
+	}
+};
+
+void sample_thread_func()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << std::this_thread::get_id() << std::endl;
+	}
+}
