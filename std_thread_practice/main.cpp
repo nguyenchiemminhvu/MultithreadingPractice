827b20433d51d837ab6a1283430d73ced9b980fb
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-03-31 16:03:13 +0700

    update

--- std_thread_practice/main.cpp
+++ std_thread_practice/main.cpp
@@ -5,7 +5,10 @@ int main()
 	std::thread t1(sample_thread_func);
 	std::thread t2((SampleWorker()));
 	
-	auto f = []() { for (int i = 0; i < 10; i++) std::cout << std::this_thread::get_id() << std::endl; };
+	auto f = []() { 
+		for (int i = 0; i < 10; i++) 
+			std::cout << std::this_thread::get_id() << std::endl; 
+	};
 	std::thread t3(f);
 
 	t1.join();
