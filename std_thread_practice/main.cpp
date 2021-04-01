962c6c01ab981b11c286ab09507ea7a6e2ab6356
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2021-04-01 10:43:25 +0700

    update

--- std_thread_practice/main.cpp
+++ std_thread_practice/main.cpp
@@ -2,30 +2,7 @@
 
 int main()
 {
-	std::thread t1(sample_thread_func);
+	sample_threads::create_sample_threads();
 
-	std::thread t2((SampleWorker()));
-	
-	auto f = []() { 
-		for (int i = 0; i < 10; i++) 
-			std::cout << std::this_thread::get_id() << std::endl; 
-		std::this_thread::sleep_for(20ms);
-	};
-	std::thread t3(f);
-
-	std::thread t4(sample_thread_func_with_params, 100);
-	
-	if (t1.joinable())
-		t1.join();
-
-	if (t2.joinable())
-		t2.join();
-
-	if (t3.joinable())
-		t3.join();
-
-	if (t4.joinable())
-		t4.detach();
-	
 	return 0;
 }
--- std_thread_practice/shared.hpp
+++ std_thread_practice/shared.hpp
@@ -6,37 +6,109 @@
 
 using namespace std;
 
-class SampleWorker
+namespace sample_threads
 {
-public:
-	void operator()()
+	class SampleWorker
+	{
+	public:
+		void operator()()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				std::cout << std::this_thread::get_id() << std::endl;
+			}
+		}
+
+		void sample_thread_worker_func(int& n)
+		{
+			n++;
+			for (int i = 0; i < n; i++)
+			{
+				std::cout << i << std::endl;
+			}
+		}
+
+		static void sample_thread_worker_static_func(int& n)
+		{
+			n++;
+			for (int i = 0; i < n; i++)
+			{
+				std::cout << i << std::endl;
+			}
+		}
+	};
+
+	void sample_thread_func()
 	{
 		for (int i = 0; i < 10; i++)
 		{
 			std::cout << std::this_thread::get_id() << std::endl;
 		}
 	}
-};
 
-void sample_thread_func()
-{
-	for (int i = 0; i < 10; i++)
+	void sample_thread_func_with_params(int n)
 	{
-		std::cout << std::this_thread::get_id() << std::endl;
-	}
-}
+		std::ofstream f;
+		f.open("test.txt", std::ios_base::out);
 
-void sample_thread_func_with_params(int n)
-{
-	std::ofstream f;
-	f.open("test.txt", std::ios_base::out);
+		for (int i = 0; i < n; i++)
+		{
+			//std::cout << i << std::endl;
+			f << i << std::endl;
+			//std::this_thread::sleep_for(200ms);
+		}
 
-	for (int i = 0; i < n; i++)
-	{
-		std::cout << i << std::endl;
-		f << i << std::endl;
-		//std::this_thread::sleep_for(200ms);
+		f.close();
 	}
 
-	f.close();
+	void sample_thread_func_with_ref_params(const int& n)
+	{
+		int& _n = const_cast<int&>(n);
+		_n++;
+	}
+
+	void create_sample_threads()
+	{
+		std::thread t1(sample_thread_func);
+
+		std::thread t2((SampleWorker()));
+
+		auto f = []() {
+			for (int i = 0; i < 10; i++)
+				std::cout << std::this_thread::get_id() << std::endl;
+			std::this_thread::sleep_for(20ms);
+		};
+		std::thread t3(f);
+
+		std::thread t4(sample_thread_func_with_params, 100);
+
+		if (t1.joinable())
+			t1.join();
+
+		if (t2.joinable())
+			t2.join();
+
+		if (t3.joinable())
+			t3.join();
+
+		if (t4.joinable())
+			t4.detach();
+
+		int n = 10;
+		std::thread t5(sample_thread_func_with_ref_params, std::ref(n));
+		if (t5.joinable())
+			t5.join();
+		std::cout << "n in main thread after joined thread: " << n << std::endl;
+
+		SampleWorker sw;
+		std::thread t6(&SampleWorker::sample_thread_worker_func, &sw, std::ref(n));
+		if (t6.joinable())
+			t6.join();
+		std::cout << "n in main thread after joined thread: " << n << std::endl;
+
+		std::thread t7(&SampleWorker::sample_thread_worker_static_func, std::ref(n));
+		if (t7.joinable())
+			t7.join();
+		std::cout << "n in main thread after joined thread: " << n << std::endl;
+	}
 }
