548131002ab6c986db7bbc21add80b812c15d040
unknown <nguyenchiemminhvu@gmail.com>
2021-04-04 19:46:20 +0700

    update

---
+++ win32_thread_practice/shared.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <stdlib.h>
+#include <stdint.h>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <Windows.h>
+#include <process.h>
+
+using namespace std;
+
---
+++ win32_thread_practice/win32_thread_practice.cpp
@@ -0,0 +1,11 @@
+// win32_thread_practice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "shared.hpp"
+
+int main()
+{
+	std::cout << "hello win32 thread practice" << std::endl;
+
+	return 0;
+}
