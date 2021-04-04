6f1aaa2d7f151b41f33ee93bb28109cb96e52adc
unknown <nguyenchiemminhvu@gmail.com>
2021-04-04 21:01:19 +0700

    update

--- win32_thread_practice/shared.hpp
+++ win32_thread_practice/shared.hpp
@@ -32,10 +32,16 @@ namespace SampleWin32Threads
 		{
 			HANDLE h_worker = (HANDLE)_beginthread(&SampleWorker::SampleThread, 0, NULL);
 			HANDLE h_worker_ex = (HANDLE)_beginthreadex(NULL, 0, &SampleWorker::SampleThreadEx, 0, 0, 0);
+			HANDLE h_worker_ex_sus = (HANDLE)_beginthreadex(NULL, 0, &SampleWorker::SampleThreadSuspended, 0, CREATE_SUSPENDED, 0);
 
 			WaitForSingleObject(h_worker, INFINITE);
 			WaitForSingleObject(h_worker_ex, INFINITE);
+
+			ResumeThread(h_worker_ex_sus);
+			WaitForSingleObject(h_worker_ex_sus, INFINITE);
+
 			CloseHandle(h_worker_ex);
+			CloseHandle(h_worker_ex_sus);
 		}
 
 		static void SampleThread(void* arg)
@@ -47,6 +53,14 @@ namespace SampleWin32Threads
 		{
 			std::cout << GetCurrentThreadId() << std::endl;
 
+			return 0;
+		}
+
+		static unsigned int __stdcall SampleThreadSuspended(void* arg)
+		{
+			std::cout << "Resume..." << std::endl;
+			std::cout << GetCurrentThreadId() << std::endl;
+			
 			return 0;
 		}
 	};
